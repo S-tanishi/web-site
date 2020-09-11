70442d51806b92487d56facb6d325d925288c563
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-11 17:10:21 +0900

    学習用にコメントを追加

--- app/Http/Controllers/TopPageController.php
+++ app/Http/Controllers/TopPageController.php
@@ -1,5 +1,6 @@
 <?php
 // 名前空間：ファイルの居場所を示す
+// 同じ関数名を使うことはできない→名前空間で衝突を回避
 namespace App\Http\Controllers;
 
 // use宣言：中で使うクラスを宣言する
@@ -26,3 +27,6 @@ class TopPageController extends Controller
  * }
  */
 
+/** 
+ * use キーワードを使うことで、名前空間の記述を短くした別名または同名を作ることができる。
+ * 別名:エイリアス、同名:インポート と呼びます。
