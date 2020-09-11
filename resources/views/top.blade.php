8142e48929e8b5c4be545a7468e37e653f7272f7
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-11 13:51:14 +0900

    トップページに表示するテンプレートを変更

---
+++ resources/views/top.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<header>
+  <h1>Web Site</h1>
+  <nav>
+    <a href="/">トップ</a>
+  </nav>
+</header>
+<main>
+  <h1>トップページだよ</h1>  
+</main>
+<footer>
+  &copy; 2020 XXXX
+</footer>
+</body>
+</html>
