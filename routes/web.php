eba704c4d5d8092ebd3a5b7db04654f59c646297
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-11 16:19:21 +0900

    コントローラー作成＆ルーティング

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('top');
-});
+// Route::get('/', function () {
+//     return view('top');
+// });
+
+Route::get('/', 'TopPageController@show');
