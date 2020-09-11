adcd8b91b671bbb5f6c859c8d81a662a6554d0ad
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-09-11 16:34:59 +0900

    概要ページ追加

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+</head>
+
+<body>
+@include("parts.header")
+<main>
+    <h1>概要ページ</h1>
+</main>
+@include("parts.footer")
+</body>
+</html>
